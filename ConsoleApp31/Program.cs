46dbb5cd1d4d4fc87a08449dbe8e8f26f5915070
GET Academy <terje@kolderup.net>
2020-04-15 14:40:27 +0200

    Initial commit

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace ConsoleApp31
+{
+    internal class Program
+    {
+        private static T[] Add<T>(T newValue, T[] oldList = null)
+        {
+            if(oldList==null)oldList = new T[0];
+            var list = new List<T>(oldList);
+            list.Add(newValue);
+            return list.ToArray();
+        }
+
+        private static void Main(string[] args)
+        {
+            var thing = new value();
+            thing.type = "subscribe";
+            thing.product_ids = Add("ETH-USD", thing.product_ids);
+            thing.product_ids = Add("ETH-EUR", thing.product_ids);
+            thing.channels = Add(new valueChannels() { name = "level2" }, thing.channels);
+            thing.channels = Add(new valueChannels() { name = "heartbeat" }, thing.channels);
+            thing.channels = Add(new valueChannels()
+            {
+                name = "ticker",
+                product_ids = new string[] { "ETH-BTC", "ETH-USD" }
+            }, thing.channels);
+            var json = JsonConvert.SerializeObject(thing);
+        }
+
+        /*
+                        {
+                            ""name"": ""ticker"",
+                            ""product_ids"": [
+                                ""ETH-BTC"",
+                                ""ETH-USD""
+                            ]
+                        }
+            var wrapperJson = "{ \"value\": " + json + "}";
+            var obj = (XmlDocument)JsonConvert.DeserializeXmlNode(wrapperJson);
+            var xml = obj.OuterXml;         
+         */
+    }
+}
